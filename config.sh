0a88dbfb372cba3e012667b9eb6e693f4c2b6657
faroke <faroke-wb@outlook.fr>
2016-05-14 11:20:24 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -31,8 +31,11 @@ Commandes :
  Debian >>> debian
 EOF
 read -p " >>>" mainchoice
+
 }
-ubuntu () { 
+sh_ubuntu () { 
+mkdir /opt/FE
+cp ./* /opt/FE
 #V1
 sudo apt-get install nikto
 sudo apt-get install nmap
@@ -40,7 +43,14 @@ sudo apt-get install subversion
 cd /opt
 sudo svn checkout http://metagoofil.googlecode.com/svn/trunk/ metagoofil
 sudo svn checkout http://theharvester.googlecode.com/svn/trunk/ theharvester
-cd
 #wget https://github.com/Arachni/arachni/releases/download/v1.4/arachni-1.4-0.5.10-linux-i686.tar.gz
 sudo echo "alias FE='sh /opt/FE/FE.sh'" >> .bashrc
 }
+menu
+if [ $mainchoice = "ubuntu" ]
+	then
+sh_ubuntu
+exit
+	else
+exit
+fi
