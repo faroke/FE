e6bc39082bab6d89effe7351db50d8f6628ca282
faroke <faroke-wb@outlook.fr>
2016-05-14 11:25:49 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -39,11 +39,10 @@ cp ./* /opt/FE
 #V1
 sudo apt-get install nikto
 sudo apt-get install nmap
-sudo apt-get install subversion
 cd /opt
-sudo svn checkout http://metagoofil.googlecode.com/svn/trunk/ metagoofil
-sudo svn checkout http://theharvester.googlecode.com/svn/trunk/ theharvester
-#wget https://github.com/Arachni/arachni/releases/download/v1.4/arachni-1.4-0.5.10-linux-i686.tar.gz
+#sudo wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/metagoofil/metagoofil-2.2.tar.gz
+#sudo wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/theharvester/theHarvester-2.2a.tar.gz
+#sudo wget https://github.com/Arachni/arachni/releases/download/v1.4/arachni-1.4-0.5.10-linux-i686.tar.gz
 sudo echo "alias FE='sh /opt/FE/FE.sh'" >> .bashrc
 }
 menu
