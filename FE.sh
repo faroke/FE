a2bc0d59708e3dc4ad91c72a576501e001d07d59
faroke <faroke-wb@outlook.fr>
2016-05-21 09:31:51 +0200

    Update FE.sh

--- FE.sh
+++ FE.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 # GNU GENERAL PUBLIC LICENSE
 # FE | AUTOMATE by 3n0k4s & F4r0k3
+vrb_all () {
+  if [ targetq = "exit"]; then
+  exit
+  else
+  echo "Error"
+  target
+  fi
+}
+}
+target () {
+  vrb_all
 echo "╔══════════════[Sh3ll]═══════════╗"
 echo "╚═╗═════════[Automaters]════════════╝"
 echo "  ╚═╗"
-read -p "    ╚═(Target)═> " menu
+read -p "    ╚═(Target)═> " targetq
+}
