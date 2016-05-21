0df6ebd7c72b755632796c02e98c238a46c1d415
faroke <faroke-wb@outlook.fr>
2016-05-21 09:17:42 +0200

    Update FE.sh

--- FE.sh
+++ FE.sh
@@ -2,3 +2,4 @@
 # GNU GENERAL PUBLIC LICENSE
 # FE | AUTOMATE by 3n0k4s & F4r0k3
  
+read -p "╔══════════════[Sh3ll]═══════════╗/n╚═╗/n═════════[Automaters]════════════╝/n  ╚═╗/n    ╚═(Target)═> " menu
