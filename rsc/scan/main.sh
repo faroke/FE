e8aa33f4c01fb8c9a55dd7fdd3b518f70feb2233
faroke <faroke-wb@outlook.fr>
2016-05-29 18:30:14 +0200

    Update main.sh

--- rsc/scan/main.sh
+++ rsc/scan/main.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 # GNU GENERAL PUBLIC LICENSE
 # FExploit by 3n0k4s & F4r0k3
+#!/bin/sh
+# GNU GENERAL PUBLIC LICENSE
+# FExploit by 3n0k4s & F4r0k3
+cat >> listool.txt
+read -p "Select your exploit >> " menu
