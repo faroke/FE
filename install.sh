8d36e08202ba388ebd5434f9bbb3b4234601f22d
faroke <faroke-wb@outlook.fr>
2016-06-03 11:02:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # GNU GENERAL PUBLIC LICENSE
 # FExploit by 3n0k4s & F4r0k3
-menu () {
 cat << EOF
                                             
                                             
@@ -21,38 +20,10 @@ FF:::::::FF           EE::::::EEEEEEEE:::::E
 F::::::::FF           E::::::::::::::::::::E
 F::::::::FF           E::::::::::::::::::::E
 FFFFFFFFFFF           EEEEEEEEEEEEEEEEEEEEEE
-                                            
-  Sélectionnez votre système                                          
-Commandes :
- Ubuntu  >>> ubuntu
- 
- Kali Linux >>> kali
-
- Debian >>> debian
 EOF
-read -p " >>>" mainchoice
-
-}
-sh_ubuntu () { 
 mkdir /opt/FExploit
 touch /opt/FExploit/FExploit.sh
 cat >> /opt/FExploit/FExploit.sh << EOF
 #######CODE#######
 EOF
-#V1
-sudo apt-get install nikto
-sudo apt-get install nmap
-cd /opt
-#sudo wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/metagoofil/metagoofil-2.2.tar.gz
-#sudo wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/theharvester/theHarvester-2.2a.tar.gz
-#sudo wget https://github.com/Arachni/arachni/releases/download/v1.4/arachni-1.4-0.5.10-linux-i686.tar.gz
 sudo echo "alias FExploit='sh /opt/FExploit/FExploit.sh'" >> .bashrc
-}
-menu
-if [ $mainchoice = "ubuntu" ]
-	then
-sh_ubuntu
-exit
-	else
-exit
-fi
